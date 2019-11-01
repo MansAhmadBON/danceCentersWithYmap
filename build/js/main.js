74341cd691c70a04e697fbac3e1315bef55c27c2
MansAhmadBONgit config --global user.name MansAhmadBONgit config --global user.name MansAhmadBON <mansue.ahmad.94@mail.ru>
2019-11-01 17:52:33 +0300

    feat: connected ymap

--- build/js/main.js
+++ build/js/main.js
@@ -1,7 +1,7 @@
 window.onload = function(){
     ymaps.ready(init);
         function init(){
-            var myMap = new ymaps.Map("map", {
+            const myMap = new ymaps.Map("map", {
                 center: [55.76, 37.64],
                 zoom: 7
             });
