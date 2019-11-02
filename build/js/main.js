ba1738e0ea2e1d2054a01e83928f14f21cb379db
MansAhmadBONgit config --global user.name MansAhmadBONgit config --global user.name MansAhmadBON <mansue.ahmad.94@mail.ru>
2019-11-02 10:46:32 +0300

    feat: implemented switching between blocks

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,23 @@
 window.onload = function(){
+    const headerBtn = document.querySelector('.headerBtn');
+    const mainContent = document.querySelector('.mainContent');
+
+    headerBtn.addEventListener('click', () => {
+        mainContent.classList.toggle('mainContent-active');
+        headerBtn.classList.toggle('headerBtn-clicked');
+    })
+
+
+
+
+
+
+
+
+
+
+
+
     ymaps.ready(init);
         function init(){
             const myMap = new ymaps.Map("map", {
