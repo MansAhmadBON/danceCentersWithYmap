e3d5605ea5a7d859bf109a784ef3c5f4432739ef
MansAhmadBONgit config --global user.name MansAhmadBONgit config --global user.name MansAhmadBON <mansue.ahmad.94@mail.ru>
2019-11-02 16:04:08 +0300

    feat: implemented menu

--- build/js/main.js
+++ build/js/main.js
@@ -1,13 +1,51 @@
 window.onload = function(){
     const headerBtn = document.querySelector('.headerBtn');
     const mainContent = document.querySelector('.mainContent');
+    const mainContentColumnRight = document.querySelector('.mainContent__column-right')
+    const menuItems = document.querySelectorAll('.menu__item');
+    const contentItems = document.querySelectorAll('.contentItem');
+
+    
 
     headerBtn.addEventListener('click', () => {
         mainContent.classList.toggle('mainContent-active');
         headerBtn.classList.toggle('headerBtn-clicked');
     })
 
+    mainContentColumnRight.addEventListener('click', (e) => OpenItemCenter(e))
+    
+    function OpenItemCenter(e){
+        let target = e.target;
+        let targetClassName = e.target.className;
 
+        contentItems.forEach(el => el.classList.remove('contentItem-active'));
+        menuItems.forEach(el => el.classList.remove('menu__item-cliked'));
+        target.classList.add('menu__item-cliked');
+
+        switch(targetClassName){
+            case 'menu__item menu__item-bizon':
+                document.querySelector('.contentItem-bizon').classList.add('contentItem-active');
+                break;
+            case 'menu__item menu__item-larry':
+                document.querySelector('.contentItem-larry').classList.add('contentItem-active');
+                break;
+            case 'menu__item menu__item-gato':
+                document.querySelector('.contentItem-gato').classList.add('contentItem-active');
+                break;
+            case 'menu__item menu__item-fly':
+                    document.querySelector('.contentItem-fly').classList.add('contentItem-active');
+                break;
+            case 'menu__item menu__item-cyb':
+                document.querySelector('.contentItem-cyb').classList.add('contentItem-active');
+            break;
+            case 'menu__item menu__item-mi':
+                document.querySelector('.contentItem-mi').classList.add('contentItem-active');
+            break;
+            default: 
+                null
+                break;
+        }
+    }
 
 
 
